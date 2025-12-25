7ca97e7201a426cb6d97ddf6c7d3e4d53f571e0d
swatibharate71 <swatibharate71@gmail.com>
2025-12-25 09:48:25 +0530

    ok

--- BachatGat/BachatGatDAL/obj/Debug/net8.0/BachatGatDAL.AssemblyInfo.cs
+++ BachatGat/BachatGatDAL/obj/Debug/net8.0/BachatGatDAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BachatGatDAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2182af190fd39278e17d981becc98be7543675")]
 [assembly: System.Reflection.AssemblyProductAttribute("BachatGatDAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BachatGatDAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
